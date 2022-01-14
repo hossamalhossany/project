c720e1bbf17119d2740a09159ccf9900ab90a6c3
hossamalhossany <hossam.2030@gmail.com>
2022-01-14 20:10:45 +0200

    3 ** 14-1-2022 -- 8:10 PM

--- home/tohy9ak93y7n/project/project/settings.py
+++ home/tohy9ak93y7n/project/project/settings.py
@@ -9,12 +9,12 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+# this second commit --- 14-1-2022 -- 8:09 PM
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# this second commit --- 14-1-2022 -- 10:42 AM
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
