909fc53f4c53f22ccb7dc38c934b4f8112f20a16
hossamalhossany <hossam.2030@gmail.com>
2022-01-15 00:10:41 +0200

    5 ** 15-1-2022 -- 12:10 AM-home

---
+++ home/tohy9ak93y7n/project/pages/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def index(request):
+    html_file = 'pages/index.html'
+    return render(request, html_file)
+
+
+def about(request):
+    return HttpResponse("ABOUT")
+
+
+def contact(request):
+    return HttpResponse("contact")
