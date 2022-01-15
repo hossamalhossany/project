8abff2c88d146831ebe2d6e4da82f1e73de53313
hossam <hossam@hossam.com>
2022-01-15 11:15:15 +0200

    8 ** 15-1-2022 ** 11:14 AM - work

--- home/tohy9ak93y7n/project/project/settings.py
+++ home/tohy9ak93y7n/project/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-# hossam make change at project @ 14-1-2022   10:18 PM
+# hossam make change at project @ 15-1-2022   11:18 PM - work
 from pathlib import Path
 import os
 
