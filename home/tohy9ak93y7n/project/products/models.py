7bc8c1619cbaff8e4fc6ef87e5bbd5425312b42f
hossam <hossam@hossam.com>
2022-01-17 15:02:25 +0200

    25 ** 17-1-2022 ** 3:00 PM - work

--- home/tohy9ak93y7n/project/products/models.py
+++ home/tohy9ak93y7n/project/products/models.py
@@ -9,3 +9,8 @@ class Product(models.Model):
     content = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering =['name']
