ac6d062d960665a521a099b5360e99245c64c8f6
hossam <hossam@hossam.com>
2022-01-17 11:48:53 +0200

    23 ** 17-1-2022 ** 11:48 AM - work

--- home/tohy9ak93y7n/project/project/settings.py
+++ home/tohy9ak93y7n/project/project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['www.hossamweb.com']
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'products.apps.ProductsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
