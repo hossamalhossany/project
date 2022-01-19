04447cae0cd718041d81aae4bb19decf1073fd3b
hossam <hossam@hossam.com>
2022-01-19 11:39:02 +0200

    31 ** 19-1-2022 ** 11:30 ِAM - work

--- home/tohy9ak93y7n/project/pages/models.py
+++ home/tohy9ak93y7n/project/pages/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Login(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.username
--- home/tohy9ak93y7n/project/pages/views.py
+++ home/tohy9ak93y7n/project/pages/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Login
+from .forms import Login_form
+
 
 
 # Create your views here.
@@ -16,12 +18,18 @@ def about(request):
     # get data from template
     username_from_temp = request.POST.get('username')
     password_from_temp = request.POST.get('password')
+
     # join data_from template to table
     data = Login(username=username_from_temp, password=password_from_temp)
+
     # save the data
     data.save()
+
+    # create form by forms.py
+    data_form = {'login_form': Login_form}
+
     html_file = 'pages/about.html'
-    return render(request, html_file)
+    return render(request, html_file, data_form)
 
 
 def contact(request):
