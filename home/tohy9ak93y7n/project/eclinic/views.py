885552a2d7854c34e17787b077974fd41968b375
hossamalhossany <hossam.2030@gmail.com>
2022-02-10 22:26:55 +0200

    50 ** 10-2-2022 -- 10:30 PM - home

--- home/tohy9ak93y7n/project/eclinic/views.py
+++ home/tohy9ak93y7n/project/eclinic/views.py
@@ -12,32 +12,24 @@ def index(request):
 
 
 def modules(request):
-    # get data from table
-    with connection.cursor() as cursor:
-        sql = ''' select username, password from project_db.eclinic_login  '''
-        cursor.execute(sql)
-        rows = cursor.fetchall()
+    if request.method == 'POST':
 
-    # end of ==> get data from table
+        username_from_temp = request.POST.get('username')
+        password_from_temp = request.POST.get('password')
 
-    # if request.method == 'POST':
-    #     data = Login(request.POST)
-    #
-    #     if data.is_valid():
-    #         pass
+        # get data from table
+        with connection.cursor() as cursor:
+            sql = ''' select username, password from project_db.eclinic_login  '''
+            cursor.execute(sql)
+            rows = cursor.fetchall()
+        # end of ==> get data from table
 
-    data_from_table = dict(rows)
+        # convert rows from tuple to dictionary
+        data_from_table = dict(rows)
 
-    username_from_temp = request.POST.get('username')
-    password_from_temp = request.POST.get('password')
-
-    if username_from_temp in data_from_table and password_from_temp in data_from_table.values():
-        result = {'ok': 'ok'}
-    else:
-        result = {'user name or password NOT Correct': 'user name or password NOT Correct'}
-
-    data_from_temp_2 = {username_from_temp: password_from_temp}
-
-    html_file = 'eclinic/modules.html'
-    return render(request, html_file,
-                  {'data_from_table': data_from_table, 'data_from_temp_2': data_from_temp_2, 'result': result})
+        if username_from_temp in data_from_table and password_from_temp in data_from_table.values():
+            html_file = 'eclinic/modules.html'
+            return render(request, html_file, )
+        else:
+            html_file = 'eclinic/login_error.html'
+            return render(request, html_file, )
