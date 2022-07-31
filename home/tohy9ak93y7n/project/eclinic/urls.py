2e2f90095fb2faecc387981491a1af4a9e4a8954
hossam <49634680+hossamalhossany@users.noreply.github.com>
2022-07-31 04:56:00 +0200

    84 ** 31-7-2022 -- 5:00 AM - home

--- home/tohy9ak93y7n/project/eclinic/urls.py
+++ home/tohy9ak93y7n/project/eclinic/urls.py
@@ -4,6 +4,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('modules', views.modules, name='modules'),
     path('modules/search_patient', views.search_patient, name='search_patient'),
+    # here we add code for search buttun
     path('',views.search_patient_data, name='search_patient_data'),
     path('modules/clinic', views.clinic, name='clinic'),
 ]
